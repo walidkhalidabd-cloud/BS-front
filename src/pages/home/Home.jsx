d5448a6cc1066ff62e4458e7219ba35b869e744c
ramaalhaj125-cloud <ramaalhaj125@gmail.com>
2025-11-27 08:49:14 +0300

    routing

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+export default function Home() {
+  return (
+    <>
+    <main>
+
+    <Outlet/>
+    </main>
+    </>
+  )
+}
---
+++ src/router.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/home/partials/Login";
+import Register from "./pages/home/partials/Register";
+import NotFound from "./pages/NotFound";
+import Main from "./pages/home/partials/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    children: [
+      {
+        index: true,
+        element: <Main />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+    ],
+  },  
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+export default router;
